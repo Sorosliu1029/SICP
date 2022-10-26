d4332e409a4f95f16c41946190dcd77afa585548
Soros Liu <soros.liu1029@gmail.com>
2022-10-26 12:22:57 +0800

    finish exercise 1.3

---
+++ exercises/1-3.scm
@@ -0,0 +1,26 @@
+;;; Soros Liu
+;;; Exercise 1.3
+
+(define (square x)
+  (* x x))
+
+(define (sum-of-square-of-2-larger a b c)
+  (cond ((and (< a b) (< a c)) (+ (square b) (square c)))
+	((and (< b a) (< b c)) (+ (square a) (square c)))
+	(else (+ (square a) (square b)))))
+
+;;; Test
+(sum-of-square-of-2-larger 1 2 3)
+;Value: 13
+
+(sum-of-square-of-2-larger 2 2 1)
+;Value: 8
+
+(sum-of-square-of-2-larger 3 2 1)
+;Value: 13
+
+(sum-of-square-of-2-larger 2 2 2)
+;Value: 8
+
+(sum-of-square-of-2-larger 4 3 5)
+;Value: 41
