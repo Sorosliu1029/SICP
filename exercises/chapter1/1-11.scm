604b7de872e5759d9ce0834de4b44c1a2c36dbc3
Soros Liu <soros.liu1029@gmail.com>
2022-10-27 21:23:23 +0800

    finish exercise 1.11

--- exercises/chapter1/1-11.scm
+++ exercises/chapter1/1-11.scm
@@ -1,3 +1,87 @@
 ;;; Soros Liu
 ;;; Exercise 1.11
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-11.html#%_thm_1.11
+
+;;; recursive process
+(define (f n)
+  (if (< n 3)
+      n
+      (+
+       (f (- n 1))
+       (* 2 (f (- n 2)))
+       (* 3 (f (- n 3))))))
+
+(f 1)
+;Value: 1
+
+(f 2)
+;Value: 2
+
+(f 3)
+;Value: 4
+
+(f 4)
+;Value: 11
+
+(f 10)
+;Value: 1892
+
+;;; iterative process
+(define (f n)
+  (define (f-iter i fn-1 fn-2 fn-3)
+    (if (> i n)
+	fn-1
+	(f-iter
+	 (+ i 1)
+	 (+
+	  fn-1
+	  (* 2 fn-2)
+	  (* 3 fn-3))
+	 fn-1
+	 fn-2)))
+  (if (< n 3) n (f-iter 3 2 1 0)))
+
+(f 1)
+;Value: 1
+
+(f 2)
+;Value: 2
+
+(f 3)
+;Value: 4
+
+(f 4)
+;Value: 11
+
+(f 10)
+;Value: 1892
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
