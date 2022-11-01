03ff53977ef3457c1b5680a5c86842910b6bf166
Soros Liu <soros.liu1029@gmail.com>
2022-11-01 15:40:12 +0800

    finish exercise 1.21

--- exercises/chapter1/1-21.scm
+++ exercises/chapter1/1-21.scm
@@ -1,3 +1,25 @@
 ;;; Soros Liu
 ;;; Exercise 1.21
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-11.html#%_thm_1.21
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (sqaure n) (* n n))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(smallest-divisor 199)
+;Value: 199
+
+(smallest-divisor 1999)
+;Value: 1999
+
+(smallest-divisor 19999)
+;Value: 7
