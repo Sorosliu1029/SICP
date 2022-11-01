2c315a24bf108a0a1de43482443ac39fdbf8b029
Soros Liu <soros.liu1029@gmail.com>
2022-11-01 12:06:24 +0800

    finish exercise 1.16

--- exercises/chapter1/1-16.scm
+++ exercises/chapter1/1-16.scm
@@ -1,3 +1,30 @@
 ;;; Soros Liu
 ;;; Exercise 1.16
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-11.html#%_thm_1.16
+
+(define (fast-expt b n)
+  (define (fast-expt-iter b n a)
+    (cond ((= n 0) a)
+	  ((even? n) (fast-expt-iter (square b) (/ n 2) a))
+	  (else (fast-expt-iter b (- n 1) (* a b)))))
+  (fast-expt-iter b n 1))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (square n) (* n n))
+
+(fast-expt 2 0)
+;Value: 1
+
+(fast-expt 2 1)
+;Value: 2
+
+(fast-expt 2 2)
+;Value: 4
+
+(fast-expt 2 3)
+;Value: 8
+
+(fast-expt 2 10)
+;Value: 1024
--- exercises/finish-exercise.sh
+++ exercises/finish-exercise.sh
@@ -5,9 +5,10 @@ CHAPTER=$(echo $1 | awk -F'.' '{print $1}')
 EXERCISE=$(echo $1 | awk -F'.' '{print $2}')
 
 FILE_PATH="$BASEDIR/chapter$CHAPTER/$CHAPTER-$EXERCISE.scm"
-
-echo -n "Git add $FILE_PATH"
-git add $FILE_PATH && echo "... Done"
+README_PATH="$BASEDIR/README.md"
 
 echo -n "Mark in README.md"
-sed -Ei "s/\:white_large_square\:( \[$CHAPTER.$EXERCISE\])/\:white_check_mark\:\1/g" $BASEDIR/README.md && echo "... Done"
+sed -Ei "s/\:white_large_square\:( \[$CHAPTER.$EXERCISE\])/\:white_check_mark\:\1/g" $README_PATH && echo " ... Done"
+
+echo -n "Git add $FILE_PATH and $README_PATH"
+git add $FILE_PATH $README_PATH && echo " ... Done"
