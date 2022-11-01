bdb0bb57e0cc0204d10391d37924d3f97dd29ad3
Soros Liu <soros.liu1029@gmail.com>
2022-11-01 12:28:12 +0800

    finish exercise 1.17

--- exercises/chapter1/1-17.scm
+++ exercises/chapter1/1-17.scm
@@ -1,3 +1,27 @@
 ;;; Soros Liu
 ;;; Exercise 1.17
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-11.html#%_thm_1.17
+
+(define (double n) (* 2 n))
+
+(define (halve n) (/ n 2))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (multiply a b)
+  (cond ((= b 0) 0)
+	((even? b) (multiply (double a) (halve b)))
+	(else (+ a (multiply a (+ b -1))))))
+
+(multiply 2 0)
+;Value: 0
+
+(multiply 2 1)
+;Value: 2
+
+(multiply 2 2)
+;Value: 4
+
+(multiply 2 11)
+;Value: 22
