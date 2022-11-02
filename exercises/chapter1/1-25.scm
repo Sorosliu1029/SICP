bb80c070eb20f7d4ff7bf5f1a4275c13b8820021
Soros Liu <soros.liu1029@gmail.com>
2022-11-02 10:17:37 +0800

    finish exercise 1.25

--- exercises/chapter1/1-25.scm
+++ exercises/chapter1/1-25.scm
@@ -1,3 +1,49 @@
 ;;; Soros Liu
 ;;; Exercise 1.25
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-11.html#%_thm_1.25
+
+(define (expmod base exp m)
+  (remainder (fast-expt base exp) m))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (square n) (* n n))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+	((fermat-test n) (fast-prime? n (- times 1)))
+	(else false)))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? n 10)
+      (report-prime (- (runtime) start-time))
+      #f))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time)
+  #t)
+
+(timed-prime-test 57793)
+; 57793 *** 1.4700000000000006
+;Value: #t
+
+; IN MATH, it is correct
+; BUT IN ENGINEERING, it cannot serve well because it's too too slow,
+; (remainder a m) needs to deal with `a` that is much LARGER than `m`, which is inefficient
