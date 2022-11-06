2d83863d12a95a66c1f4bc1de5c9b204849bfada
Soros Liu <soros.liu1029@gmail.com>
2022-11-06 16:45:02 +0800

    finish exercise 2.9

--- exercises/chapter2/2-9.scm
+++ exercises/chapter2/2-9.scm
@@ -1,3 +1,47 @@
 ;;; Soros Liu
 ;;; Exercise 2.9
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-14.html#%_thm_2.9
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+;;; the width of the sum of two intervals is a function only of
+;;; the widths of the intervals being added
+
+(width (add-interval x y))
+;;; => (width (make-interval (+ (lower-bound x) (lower-bound y))
+;;; => 		      (+ (upper-bound x) (upper-bound y))))
+;;; => (/ (-
+;;; =>     (upper-bound (make-interval (+ (lower-bound x) (lower-bound y))
+;;; => 				(+ (upper-bound x) (upper-bound y))))
+;;; =>     (lower-bound (make-interval (+ (lower-bound x) (lower-bound y))
+;;; => 				(+ (upper-bound x) (upper-bound y))))
+;;; =>     ) 2)
+;;; => (/ (-
+;;; =>     (+ (upper-bound x) (upper-bound y))
+;;; =>     (+ (lower-bound x) (lower-bound y))
+;;; =>     ) 2)
+;;; => (/ (+ (- (upper-bound x) (lower-bound x))
+;;; =>       (- (upper-bound y) (lower-bound y)))
+;;; =>    2)
+;;; => (+ (/ (- (upper-bound x) (lower-bound x)) 2)
+;;; =>    (/ (- (upper-bound y) (lower-bound y)) 2))
+;;; => (+ (width x) (width y))
+
+;;; not true for multiplication
+
+(width (mul-interval x y))
+;;; suppose x and y both is in positive range
+;;; => (width (make-interval (* (lower-bound x) (lower-bound y))
+;;; =>                       (* (upper-bound x) (upper-bound y))))
+;;; => (/ (-
+;;; =>     (upper-bound (make-interval (* (lower-bound x) (lower-bound y))
+;;; => 				(* (upper-bound x) (upper-bound y))))
+;;; =>     (lower-bound (make-interval (* (lower-bound x) (lower-bound y))
+;;; => 				(* (upper-bound x) (upper-bound y))))
+;;; =>     ) 2)
+;;; => (/ (-
+;;; =>     (* (upper-bound x) (upper-bound y))
+;;; =>     (* (lower-bound x) (lower-bound y))
+;;; =>     ) 2)
+;;; not fuction only of the widths of the intervals being multiplied
