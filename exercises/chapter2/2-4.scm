974d55e3b9ab6a8568b5d520ea7bcc3df188f33b
Soros Liu <soros.liu1029@gmail.com>
2022-11-06 12:22:00 +0800

    finish exercise 2.4

--- exercises/chapter2/2-4.scm
+++ exercises/chapter2/2-4.scm
@@ -1,3 +1,27 @@
 ;;; Soros Liu
 ;;; Exercise 2.4
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-14.html#%_thm_2.4
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define x 1)
+(define y 2)
+
+(car (cons x y))
+;Value: 1
+
+;;; (car (cons x y))
+;;; => (car (lambda (m) (m x y)))
+;;; => ((lambda (m) (m x y)) (lambda (p q) p))
+;;; => ((lambda (p q) p) x y)
+;;; => x
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(cdr (cons x y))
+;Value: 2
