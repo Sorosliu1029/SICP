ccea3ad05125d7d47e79ee4faf36f22d64a6c2fa
Soros Liu <soros.liu1029@gmail.com>
2022-11-06 16:17:50 +0800

    finish exercise 2.8

--- exercises/chapter2/2-8.scm
+++ exercises/chapter2/2-8.scm
@@ -1,3 +1,9 @@
 ;;; Soros Liu
 ;;; Exercise 2.8
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-14.html#%_thm_2.8
+
+;;; the lower bound of subtraction is lower of first interval minus upper of second interval
+;;; the upper bound of subtraction is upper of first interval minus lower of second interval
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+		 (- (upper-bound x) (lower-bound y))))
