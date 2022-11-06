2edf570ca27aa04de49e4722f1ac268720d1869f
Soros Liu <soros.liu1029@gmail.com>
2022-11-06 20:30:29 +0800

    finish exercise 2.16

--- exercises/chapter2/2-16.scm
+++ exercises/chapter2/2-16.scm
@@ -1,3 +1,10 @@
 ;;; Soros Liu
 ;;; Exercise 2.16
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-14.html#%_thm_2.16
+
+;;; Although algebraically equivalent, the occurence of uncertain number's
+;;; operation is different, which means different process of accumulation of
+;;; percentage, and it will result into answers with different error bounds
+
+;;; it seems POSSIBILE, if we can delay actual uncertain compuation to the final
+;;; and necessary stage, by applying algebraic simplification first and as much as possibile
