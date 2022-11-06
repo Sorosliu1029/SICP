34e0a71a732252f6a0d61d90161b47eb7e2625c0
Soros Liu <soros.liu1029@gmail.com>
2022-11-06 16:11:44 +0800

    finish exercise 2.7

--- exercises/chapter2/2-7.scm
+++ exercises/chapter2/2-7.scm
@@ -1,3 +1,17 @@
 ;;; Soros Liu
 ;;; Exercise 2.7
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-14.html#%_thm_2.7
+
+(define (make-interval a b) (cons a b))
+
+(define (upper-bound i) (max (car i) (cdr i)))
+
+(define (lower-bound i) (min (car i) (cdr i)))
+
+(define i (make-interval 1 2))
+
+(upper-bound i)
+;Value: 2
+
+(lower-bound i)
+;Value: 1
