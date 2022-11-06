640911ef1d06e17af6ebb5720d7f570005d2605a
Soros Liu <soros.liu1029@gmail.com>
2022-11-06 20:21:33 +0800

    finish exercise 2.15

--- exercises/chapter2/2-15.scm
+++ exercises/chapter2/2-15.scm
@@ -1,3 +1,8 @@
 ;;; Soros Liu
 ;;; Exercise 2.15
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-14.html#%_thm_2.15
+
+;;; Eva is right
+
+;;; By reducing the occurence of uncertain numbers, it can reduce the chance of
+;;; the accumulation of percentage, thus produce a tighter error bounds
