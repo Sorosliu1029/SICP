5313e341ea593e5ad41df4ccad2ab28163a6ccf4
Soros Liu <soros.liu1029@gmail.com>
2022-11-06 16:52:56 +0800

    finish exercise 2.10

--- exercises/chapter2/2-10.scm
+++ exercises/chapter2/2-10.scm
@@ -1,3 +1,13 @@
 ;;; Soros Liu
 ;;; Exercise 2.10
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-14.html#%_thm_2.10
+
+(define (div-interval x y)
+  (if (span-zero? y)
+      (error "divied by zero-span interval" y)
+      (mul-interval x
+		    (make-interval (/ 1.0 (upper-bound y))
+				   (/ 1.0 (lower-bound y))))))
+
+(define (span-zero? x)
+  (= (- (upper-bound x) (lower-bound y)) 0))
