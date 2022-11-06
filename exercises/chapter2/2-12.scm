3070ff49d14529cf347e114575002be0d2eaac9e
Soros Liu <soros.liu1029@gmail.com>
2022-11-06 18:32:43 +0800

    finish exercise 2.12

--- exercises/chapter2/2-12.scm
+++ exercises/chapter2/2-12.scm
@@ -1,3 +1,28 @@
 ;;; Soros Liu
 ;;; Exercise 2.12
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-14.html#%_thm_2.12
+
+(define (make-center-percent c p)
+  (make-interval (- c (* c p)) (+ c (* c p))))
+
+(define (percent i)
+  (/ (width i) (center i)))
+
+(define (make-interval a b) (cons a b))
+(define (upper-bound i) (cdr i))
+(define (lower-bound i) (car i))
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(define x (make-center-percent 6.8 0.1))
+
+x
+;Value: (6.12 . 7.4799999999999995)
+
+(percent x)
+;Value: .09999999999999996
+
+(center x)
+;Value: 6.8
