d1e249433ee21e4a7e5a36afd00699eb79a1b0f8
Soros Liu <soros.liu1029@gmail.com>
2022-11-07 16:36:17 +0800

    finish exercise 1.30

--- exercises/chapter1/1-30.scm
+++ exercises/chapter1/1-30.scm
@@ -1,3 +1,49 @@
 ;;; Soros Liu
 ;;; Exercise 1.30
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-12.html#%_thm_1.30
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+;;; Using sum
+
+(define (inc n) (+ n 1))
+
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(sum-cubes 1 10)
+;Value: 3025
+
+(define (identity x) x)
+
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+(sum-integers 1 10)
+;Value: 55
+
+(define (pi-sum a b)
+  (define (pi-term x)
+    (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x)
+    (+ x 4))
+  (sum pi-term a pi-next b))
+
+(* 8 (pi-sum 1 1000))
+;Value: 3.139592655589782
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2)) add-dx b)
+     dx))
+
+(integral cube 0 1 0.01)
+;Value: .24998750000000042
+
+(integral cube 0 1 0.001)
+;Value: .24999987500000073
