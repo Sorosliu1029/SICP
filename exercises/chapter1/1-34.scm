fee3cc299c93ac75751e58ca82fae4f57ad1bcf4
Soros Liu <soros.liu1029@gmail.com>
2022-11-08 11:22:18 +0800

    finish exercise 1.34

--- exercises/chapter1/1-34.scm
+++ exercises/chapter1/1-34.scm
@@ -1,3 +1,23 @@
 ;;; Soros Liu
 ;;; Exercise 1.34
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-12.html#%_thm_1.34
+
+(define (f g)
+  (g 2))
+
+(f square)
+;Value: 4
+
+(f (lambda (z) (* z (+ z 1))))
+;Value: 6
+
+(f f)
+;The object 2 is not applicable.
+
+;;; (f f) results an error
+(f f)
+;;; =>
+(f 2)
+;;; =>
+(2 2)
+;;; => error: object 2 is not applicable
