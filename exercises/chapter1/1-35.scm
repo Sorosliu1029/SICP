29dc3d30b96ad571c8bcd90431baae62570d3213
Soros Liu <soros.liu1029@gmail.com>
2022-11-09 11:16:05 +0800

    finish exercise 1.35

--- exercises/chapter1/1-35.scm
+++ exercises/chapter1/1-35.scm
@@ -1,3 +1,24 @@
 ;;; Soros Liu
 ;;; Exercise 1.35
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-12.html#%_thm_1.35
+
+;;; golden ratio phi: phi^2 = phi + 1
+;;; for function f(x) = 1 + 1/x, when x = phi
+;;; f(phi) = 1 + 1/phi = 1 + 1/(phi^2-1) = (phi^2) / (phi^2-1)
+;;; = (phi^2) / phi = phi
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(fixed-point (lambda (x) (1+ (/ 1 x)))
+             1.0)
+;Value: 1.6180327868852458
