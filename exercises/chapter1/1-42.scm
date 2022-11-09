fa807f2b71c520c5091f8517108e5c48e36832c8
Soros Liu <soros.liu1029@gmail.com>
2022-11-09 16:47:27 +0800

    finish exercise 1.42

--- exercises/chapter1/1-42.scm
+++ exercises/chapter1/1-42.scm
@@ -1,3 +1,11 @@
 ;;; Soros Liu
 ;;; Exercise 1.42
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-12.html#%_thm_1.42
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define inc 1+)
+
+((compose square inc) 6)
+;Value: 49
