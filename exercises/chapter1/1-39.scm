18500696c4a335f063c5301208e18aa0e40fe733
Soros Liu <soros.liu1029@gmail.com>
2022-11-09 12:31:16 +0800

    finish exercise 1.39

--- exercises/chapter1/1-39.scm
+++ exercises/chapter1/1-39.scm
@@ -1,3 +1,22 @@
 ;;; Soros Liu
 ;;; Exercise 1.39
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-12.html#%_thm_1.39
+
+(define (cont-frac n d k)
+  (define (recur i)
+    (if (> i k)
+	0
+	(/ (n i)
+	   (+ (d i) (recur (1+ i))))))
+  (recur 1))
+
+(define (tan-cf x k)
+  (cont-frac (lambda (i)
+               (if (= i 1) x (- (square x))))
+             (lambda (i) (-1+ (* 2 i)))
+             k))
+
+;;; tan(1) ~= 1.55740772465
+
+(tan-cf 1.0 10)
+;Value: 1.557407724654902
