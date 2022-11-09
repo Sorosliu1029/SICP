22dfc3a68c9178c984c4f08abc94cd3d29d1d042
Soros Liu <soros.liu1029@gmail.com>
2022-11-09 16:54:13 +0800

    finish exercise 1.43

--- exercises/chapter1/1-43.scm
+++ exercises/chapter1/1-43.scm
@@ -1,3 +1,14 @@
 ;;; Soros Liu
 ;;; Exercise 1.43
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-12.html#%_thm_1.43
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (repeated f n)
+  (if (= n 1)
+      f
+      (compose f (repeated f (-1+ n)))))
+
+((repeated square 2) 5)
+;Value: 625
