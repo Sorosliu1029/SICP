59870ba07794a94b5383a8d2120a4ff0c9a149b1
Soros Liu <soros.liu1029@gmail.com>
2022-11-16 23:06:53 +0800

    finish exercise 2.24

--- exercises/chapter2/2-24.scm
+++ exercises/chapter2/2-24.scm
@@ -1,3 +1,27 @@
 ;;; Soros Liu
 ;;; Exercise 2.24
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.24
+
+(list 1 (list 2 (list 3 4)))
+;Value: (1 (2 (3 4)))
+
+;;; box-and-pointer structure
+; (1 (2 (3 4))) -> |*|*|->|*|/|
+;                   |      |
+;                   1      ->|*|*|->|*|/|
+;                             |      |
+;                             2      ->|*|*|->|*|/|
+;                                       |      |
+;                                       3      4
+
+;;; tree structure
+; (1 (2 (3 4)))
+;    /    \
+;   /      \
+;  1     (2 (3 4))
+;          /  \
+;         /    \
+;        2    (3 4)
+;              /  \
+;             /    \
+;            3      4
