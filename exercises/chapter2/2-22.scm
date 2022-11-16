c4988264c2b8675b00e497f468da572c51fab3e5
Soros Liu <soros.liu1029@gmail.com>
2022-11-16 18:07:12 +0800

    finish exercise 2.22

--- exercises/chapter2/2-22.scm
+++ exercises/chapter2/2-22.scm
@@ -1,3 +1,33 @@
 ;;; Soros Liu
 ;;; Exercise 2.22
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.22
+
+(define nil ())
+
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things) 
+              (cons (square (car things))
+                    answer))))
+  (iter items nil))
+
+(square-list (list 1 2 3 4))
+;Value: (16 9 4 1)
+
+;;; as `answer` accumulating, it collects car of items first, then append rest in front
+
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons answer
+                    (square (car things))))))
+  (iter items nil))
+
+(square-list (list 1 2 3 4))
+;Value: ((((() . 1) . 4) . 9) . 16)
+
+;;; `answer` is a list, but (cons list element) does not produce a list
