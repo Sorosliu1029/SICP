f5dda59132657d0beb19d5f55244f7c725771b3b
Soros Liu <soros.liu1029@gmail.com>
2022-11-16 17:06:27 +0800

    finish exercise 2.17

--- exercises/chapter2/2-17.scm
+++ exercises/chapter2/2-17.scm
@@ -1,3 +1,14 @@
 ;;; Soros Liu
 ;;; Exercise 2.17
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.17
+
+(define (last-pair items)
+  (if (null? (cdr items))
+    items
+    (last-pair (cdr items))))
+
+(last-pair (list 23 72 149 34))
+;Value: (34)
+
+(last-pair (list 23))
+;Value: (23)
