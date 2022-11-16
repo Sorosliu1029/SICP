6e6337fc6fb60cc61d4fed8d7d786992729dc740
Soros Liu <soros.liu1029@gmail.com>
2022-11-16 23:40:07 +0800

    finish exercise 2.27

--- exercises/chapter2/2-27.scm
+++ exercises/chapter2/2-27.scm
@@ -1,3 +1,38 @@
 ;;; Soros Liu
 ;;; Exercise 2.27
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.27
+
+(define x (list (list 1 2) (list 3 4)))
+x
+;Value: ((1 2) (3 4))
+
+;;; reverse
+(define (reverse items)
+  (define (iter items acc)
+    (if (null? items)
+      acc
+      (iter (cdr items) (cons (car items) acc))))
+  (iter items ()))
+
+(reverse x)
+;Value: ((3 4) (1 2))
+
+;;; deep-reverse
+(define (deep-reverse items)
+  (define (iter x acc)
+    (cond ((null? x) acc)
+          ((not (pair? x)) x)
+          (else (iter (cdr x) (cons (deep-reverse (car x)) acc)))))
+  (iter items ()))
+
+(deep-reverse x)
+;Value: ((4 3) (2 1))
+
+(deep-reverse (list ))
+;Value: ()
+(deep-reverse (list 1))
+;Value: (1)
+(deep-reverse (list 1 2 (list 3 4)))
+;Value: ((4 3) 2 1)
+(deep-reverse (list 1 (list 2 (list 3 4))))
+;Value: (((4 3) 2) 1)
