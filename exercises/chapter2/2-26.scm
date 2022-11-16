e6cdf18ede2843d7bdaf3bdbab9b4038d85c6f68
Soros Liu <soros.liu1029@gmail.com>
2022-11-16 23:22:37 +0800

    finish exercise 2.26

--- exercises/chapter2/2-26.scm
+++ exercises/chapter2/2-26.scm
@@ -1,3 +1,15 @@
 ;;; Soros Liu
 ;;; Exercise 2.26
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.26
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(append x y)
+;Value: (1 2 3 4 5 6)
+
+(cons x y)
+;Value: ((1 2 3) 4 5 6)
+
+(list x y)
+;Value: ((1 2 3) (4 5 6))
