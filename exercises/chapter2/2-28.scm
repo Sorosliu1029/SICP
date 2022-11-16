0c616c8c18cb467dddebf019d41a18f5978557b0
Soros Liu <soros.liu1029@gmail.com>
2022-11-16 23:51:19 +0800

    finish exercise 2.28

--- exercises/chapter2/2-28.scm
+++ exercises/chapter2/2-28.scm
@@ -1,3 +1,16 @@
 ;;; Soros Liu
 ;;; Exercise 2.28
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.28
+
+(define (fringe tree)
+  (cond ((null? tree) ())
+        ((not (pair? tree)) (list tree))
+        (else (append (fringe (car tree)) (fringe (cdr tree))))))
+
+(define x (list (list 1 2) (list 3 4)))
+
+(fringe x)
+;Value: (1 2 3 4)
+
+(fringe (list x x))
+;Value: (1 2 3 4 1 2 3 4)
