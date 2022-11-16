0b82bfec6c585658e7835b81c2236ab853016b8b
Soros Liu <soros.liu1029@gmail.com>
2022-11-16 17:13:04 +0800

    finish exercise 2.18

--- exercises/chapter2/2-18.scm
+++ exercises/chapter2/2-18.scm
@@ -1,3 +1,19 @@
 ;;; Soros Liu
 ;;; Exercise 2.18
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.18
+
+(define (reverse items)
+  (define (iter items acc)
+    (if (null? items)
+      acc
+      (iter (cdr items) (cons (car items) acc))))
+  (iter items ()))
+
+(reverse (list 1 4 9 16 25))
+;Value: (25 16 9 4 1)
+
+(reverse (list 10))
+;Value: (10)
+
+(reverse (list ))
+;Value: ()
