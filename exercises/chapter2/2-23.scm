eae3e90318d97573bbb7013e94633569fd16af44
Soros Liu <soros.liu1029@gmail.com>
2022-11-16 19:16:36 +0800

    finish exercise 2.23

--- exercises/chapter2/2-23.scm
+++ exercises/chapter2/2-23.scm
@@ -1,3 +1,15 @@
 ;;; Soros Liu
 ;;; Exercise 2.23
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.23
+
+(define (for-each proc items)
+  (cond ((not (null? items)) 
+         (proc (car items))
+         (for-each proc (cdr items)))))
+
+(for-each (lambda (x) (newline) (display x))
+          (list 57 321 88))
+57
+321
+88
+;Unspecified return value
