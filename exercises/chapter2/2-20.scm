10e858d34f6ac66fee188a337a20557c0108d243
Soros Liu <soros.liu1029@gmail.com>
2022-11-16 17:43:35 +0800

    finish exercise 2.20

--- exercises/chapter2/2-20.scm
+++ exercises/chapter2/2-20.scm
@@ -1,3 +1,17 @@
 ;;; Soros Liu
 ;;; Exercise 2.20
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.20
+
+(define (same-parity a . others)
+  (define (is-same-parity a b) (boolean=? (odd? a) (odd? b)))
+  (define (recur items)
+    (cond ((null? items) ())
+          ((is-same-parity a (car items)) (cons (car items) (recur (cdr items))))
+          (else (recur (cdr items)))))
+  (cons a (recur others)))
+
+(same-parity 1 2 3 4 5 6 7)
+;Value: (1 3 5 7)
+
+(same-parity 2 3 4 5 6 7)
+;Value: (2 4 6)
