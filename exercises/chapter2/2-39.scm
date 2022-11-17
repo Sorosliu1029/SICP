e81ae8e4b4b23bb250ebb6bd5eccdddef75fee06
Soros Liu <soros.liu1029@gmail.com>
2022-11-17 19:57:46 +0800

    finish exercise 2.39

--- exercises/chapter2/2-39.scm
+++ exercises/chapter2/2-39.scm
@@ -1,3 +1,33 @@
 ;;; Soros Liu
 ;;; Exercise 2.39
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.39
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define fold-right accumulate)
+
+(define nil ())
+
+(define l (list 1 2 3 4))
+
+(define (reverse sequence)
+  (fold-right (lambda (x y) (append y (list x))) nil sequence))
+(reverse l)
+;Value: (4 3 2 1)
+
+(define (reverse sequence)
+  (fold-left (lambda (x y) (cons y x)) nil sequence))
+(reverse l)
+;Value: (4 3 2 1)
