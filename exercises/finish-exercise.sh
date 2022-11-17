47cd30198d5052c618bfb7fc5377c6a075d2f90f
Soros Liu <soros.liu1029@gmail.com>
2022-11-17 14:20:48 +0800

    update finish exercise script

--- exercises/finish-exercise.sh
+++ exercises/finish-exercise.sh
@@ -12,3 +12,14 @@ sed -Ei "s/\:white_large_square\:( \[$CHAPTER.$EXERCISE\])/\:white_check_mark\:\
 
 echo -n "Git add $FILE_PATH and $README_PATH"
 git add $FILE_PATH $README_PATH && echo " ... Done"
+
+# check if file is staged
+git diff --cached --exit-code --quiet $FILE_PATH
+# if not staged, exit and error
+if [ $? -ne 1 ]; then
+  echo "Error: $FILE_PATH is not staged"
+  exit 1
+fi
+
+echo "Git commit"
+git commit -m "finish exercise $CHAPTER.$EXERCISE" && echo "✅ Done"
