da99b89988c2fa11c0a157276891b03daaec3b92
Soros Liu <soros.liu1029@gmail.com>
2022-11-17 15:49:13 +0800

    finish exercise 2.35

--- exercises/chapter2/2-35.scm
+++ exercises/chapter2/2-35.scm
@@ -1,3 +1,25 @@
 ;;; Soros Liu
 ;;; Exercise 2.35
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.35
+
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (i) 1) (enumerate-tree t))))
+
+(define nil ())
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(define x (cons (list 1 2) (list 3 4)))
+
+(count-leaves x)
+;Value: 4
