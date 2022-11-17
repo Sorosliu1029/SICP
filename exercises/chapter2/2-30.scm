5b01ff07ffe854d22bb219a46dbb2debe977ac73
Soros Liu <soros.liu1029@gmail.com>
2022-11-17 12:26:18 +0800

    finish exercise 2.30

--- exercises/chapter2/2-30.scm
+++ exercises/chapter2/2-30.scm
@@ -1,3 +1,28 @@
 ;;; Soros Liu
 ;;; Exercise 2.30
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.30
+
+(define (square-tree tree)
+  (cond ((null? tree) ())
+        ((not (pair? tree)) (square tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+
+(square-tree
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
+;Value: (1 (4 (9 16) 25) (36 49))
+
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+	     (square-tree sub-tree)
+	     (square sub-tree)))
+       tree))
+
+(square-tree
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
+;Value: (1 (4 (9 16) 25) (36 49))
