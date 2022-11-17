3ad68f754f4cb9c39c41c6d9e09ae13023b440ff
Soros Liu <soros.liu1029@gmail.com>
2022-11-17 15:42:31 +0800

    finish exercise 2.34

--- exercises/chapter2/2-34.scm
+++ exercises/chapter2/2-34.scm
@@ -1,3 +1,17 @@
 ;;; Soros Liu
 ;;; Exercise 2.34
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.34
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms) (+ this-coeff (* higher-terms x)))
+              0
+              coefficient-sequence))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(horner-eval 2 (list 1 3 0 5 0 1))
+;Value: 79
