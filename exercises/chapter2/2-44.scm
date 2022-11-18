2658bb72ac36cffd2b31419b9acbfc7c456c304f
Soros Liu <soros.liu1029@gmail.com>
2022-11-18 15:15:45 +0800

    finish exercise 2.44

--- exercises/chapter2/2-44.scm
+++ exercises/chapter2/2-44.scm
@@ -1,3 +1,9 @@
 ;;; Soros Liu
 ;;; Exercise 2.44
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.44
+
+(define (up-split painter n)
+  (if (= n 0)
+    painter
+    (let ((smaller (up-split painter (-1+ n))))
+      (below painter (beside smaller smaller)))))
