097d991f073ec731ba68d2b9bcf192e9c50de84f
Soros Liu <soros.liu1029@gmail.com>
2022-11-18 18:38:24 +0800

    finish exercise 2.51

--- exercises/chapter2/2-51.scm
+++ exercises/chapter2/2-51.scm
@@ -1,3 +1,24 @@
 ;;; Soros Liu
 ;;; Exercise 2.51
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.51
+
+;;; analogous to beside
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-bottom
+            (transform-painter painter1
+                               (make-vect 0.0 0.0)
+                               (make-vect 1.0 0.0)
+                               split-point))
+          (paint-top
+            (transform-painter painter2
+                               split-point
+                               (make-vect 1.0 0.5)
+                               (make-vect 0.0 1.0))))
+      (lambda (frame)
+        (paint-bottom frame)
+        (paint-top frame)))))
+
+;;; with beside and rotation
+(define (below painter1 painter2)
+  (compose rotate90 (beside painter1 painter2)))
