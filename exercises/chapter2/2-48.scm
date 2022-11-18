c16976fd139e15b82378b3da7638e8bd8f884f61
Soros Liu <soros.liu1029@gmail.com>
2022-11-18 16:45:55 +0800

    finish exercise 2.48

--- exercises/chapter2/2-48.scm
+++ exercises/chapter2/2-48.scm
@@ -1,3 +1,7 @@
 ;;; Soros Liu
 ;;; Exercise 2.48
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.48
+
+(define make-segment cons)
+(define start-segment car)
+(define end-segment cdr)
