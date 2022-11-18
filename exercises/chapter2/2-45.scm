27425df7fcf4f877cb8acb73e98673d32e6b4fc5
Soros Liu <soros.liu1029@gmail.com>
2022-11-18 15:32:33 +0800

    finish exercise 2.45

--- exercises/chapter2/2-45.scm
+++ exercises/chapter2/2-45.scm
@@ -1,3 +1,13 @@
 ;;; Soros Liu
 ;;; Exercise 2.45
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.45
+
+(define (split outter-op inner-op)
+  (lambda (painter n)
+    (if (= n 0)
+      painter
+      (let ((smaller ((split outter-op inner-op) painter (-1+ n))))
+        (outter-op painter (inner-op smaller smaller))))))
+
+(define right-split (split beside below))
+(define up-split (split below beside))
