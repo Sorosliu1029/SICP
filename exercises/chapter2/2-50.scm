90f22c2e7e30052e0e92e54ecbee10b02cdca4b3
Soros Liu <soros.liu1029@gmail.com>
2022-11-18 18:28:55 +0800

    finish exercise 2.50

--- exercises/chapter2/2-50.scm
+++ exercises/chapter2/2-50.scm
@@ -1,3 +1,15 @@
 ;;; Soros Liu
 ;;; Exercise 2.50
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.50
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate180 painter)
+  (repeated rotate90 2))
+
+(define (rotate270 painter)
+  (repeated rotate90 3))
