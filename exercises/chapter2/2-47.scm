1ed4374e8f69f2553ef505b10bc5a9d3a158fdf8
Soros Liu <soros.liu1029@gmail.com>
2022-11-18 16:06:30 +0800

    finish exercise 2.47

--- exercises/chapter2/2-47.scm
+++ exercises/chapter2/2-47.scm
@@ -1,3 +1,20 @@
 ;;; Soros Liu
 ;;; Exercise 2.47
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-15.html#%_thm_2.47
+
+;;; constructor 1
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define origin-frame car)
+(define edge1-frame cadr)
+(define edge2-frame caddr)
+
+;;; constructor 2
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define origin-frame car)
+(define edge1-frame cadr)
+(define edge2-frame cddr)
+
