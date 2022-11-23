7528fe9ef38a278dbdb5c5d7f75ebe39d8735c61
Soros Liu <soros.liu1029@gmail.com>
2022-11-23 12:32:05 +0800

    finish exercise 2.54

--- exercises/chapter2/2-54.scm
+++ exercises/chapter2/2-54.scm
@@ -1,3 +1,30 @@
 ;;; Soros Liu
 ;;; Exercise 2.54
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-16.html#%_thm_2.54
+
+(define (equal? a b)
+  (or (and (not (pair? a))
+           (not (pair? b))
+           (eq? a b))
+      (and (pair? a)
+           (pair? b)
+           (equal? (car a) (car b))
+           (equal? (cdr a) (cdr b)))))
+
+(equal? '(this is a list) '(this is a list))
+;Value: #t
+
+(equal? '(this is a list) '(this (is a) list))
+;Value: #f
+
+(equal? '() '())
+;Value: #t
+
+(equal? '() 'a)
+;Value: #f
+
+(equal? 'a 'a)
+;Value: #t
+
+(equal? 1 1)
+;Value: #t
