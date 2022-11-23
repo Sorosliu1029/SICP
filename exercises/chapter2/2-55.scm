766b2d776d4765ec084936ba9e7f5b57a1110cd5
Soros Liu <soros.liu1029@gmail.com>
2022-11-23 12:39:15 +0800

    finish exercise 2.55

--- exercises/chapter2/2-55.scm
+++ exercises/chapter2/2-55.scm
@@ -1,3 +1,16 @@
 ;;; Soros Liu
 ;;; Exercise 2.55
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-16.html#%_thm_2.55
+
+(car ''abracadabra)
+;Value: quote
+
+(car ''abracadabra) ;;; =>
+(car '(quote abracadabra)) ;;; =>
+;Value: quote
+
+(cdr '(quote abracadabra))
+;Value: (abracadabra)
+
+(cadr '(quote abracadabra))
+;Value: abracadabra
