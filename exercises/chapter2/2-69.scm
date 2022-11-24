7a722c6c68016b62351a6c0dda9518e7aa5b1377
Soros Liu <soros.liu1029@gmail.com>
2022-11-24 17:44:57 +0800

    finish exercise 2.69

--- exercises/chapter2/2-69.scm
+++ exercises/chapter2/2-69.scm
@@ -1,3 +1,72 @@
 ;;; Soros Liu
 ;;; Exercise 2.69
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-16.html#%_thm_2.69
+
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(define (successive-merge pairs)
+  (if (= (length pairs) 1)
+    (car pairs)
+    (successive-merge (adjoin-set (make-code-tree (first pairs)
+                                                  (second pairs))
+                                  (drop pairs 2)))))
+
+;; representing
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch tree) (car tree))
+
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+;; sets
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)
+                               (cadr pair))
+                    (make-leaf-set (cdr pairs))))))
+
+;;; verify
+(generate-huffman-tree '((A 4) (B 2) (C 1) (D 1)))
+;Value: ((leaf a 4) ((leaf b 2) ((leaf d 1) (leaf c 1) (d c) 2) (b d c) 4) (a b d c) 8)
+
+(make-code-tree (make-leaf 'A 4)
+                (make-code-tree
+                 (make-leaf 'B 2)
+                 (make-code-tree (make-leaf 'D 1)
+                                 (make-leaf 'C 1))))
+;Value: ((leaf a 4) ((leaf b 2) ((leaf d 1) (leaf c 1) (d c) 2) (b d c) 4) (a b d c) 8)
