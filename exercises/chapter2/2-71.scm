aa1716dc25bdef09c51cd43ab96e3e478c79ee27
Soros Liu <soros.liu1029@gmail.com>
2022-11-24 18:08:07 +0800

    finish exercise 2.71

--- exercises/chapter2/2-71.scm
+++ exercises/chapter2/2-71.scm
@@ -1,3 +1,52 @@
 ;;; Soros Liu
 ;;; Exercise 2.71
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-16.html#%_thm_2.71
+
+;;; n=5
+;      w31
+;     /  \
+;    /    \
+;   16    w15
+;        /  \
+;       /    \
+;      8     w7
+;           /  \
+;          /    \
+;         4     w3
+;              /  \
+;             /    \
+;            2      1
+
+;;; n=10
+;    w1023
+;    /  \
+;   /    \
+; 512   w511
+;       /  \
+;      /    \
+;    256   w255
+;          /  \
+;         /    \
+;        128  w127
+;             /  \
+;            /    \
+;           64   w63
+;                /  \
+;               /    \
+;              32    w31
+;                    /  \
+;                   /    \
+;                  16   w15
+;                       /  \
+;                      /    \
+;                     8     w7
+;                          /  \
+;                         /    \
+;                        4     w3
+;                              / \
+;                             /   \
+;                            2     1
+
+;;; for general n
+; bits for most frequent symbol: 1
+; bits for least frequent symbol: n-1
