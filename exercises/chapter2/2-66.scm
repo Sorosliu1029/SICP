f67260b049ff63b017903cec15e68bfe1f6bb653
Soros Liu <soros.liu1029@gmail.com>
2022-11-24 15:52:21 +0800

    finish exercise 2.66

--- exercises/chapter2/2-66.scm
+++ exercises/chapter2/2-66.scm
@@ -1,3 +1,11 @@
 ;;; Soros Liu
 ;;; Exercise 2.66
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-16.html#%_thm_2.66
+
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records) false)
+        ((equal? given-key (key (entry set-of-records))) (entry set-of-records))
+        ((< given-key (key (entry set-of-records)))
+         (lookup given-key (left-branch set-of-records)))
+        ((> given-key (key (entry set-of-records)))
+         (lookup given-key (right-branch set-of-records)))))
