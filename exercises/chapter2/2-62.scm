53dd07ebed21dbc81ceb62a50e70d3f215f0126c
Soros Liu <soros.liu1029@gmail.com>
2022-11-24 12:11:39 +0800

    finish exercise 2.62

--- exercises/chapter2/2-62.scm
+++ exercises/chapter2/2-62.scm
@@ -1,3 +1,28 @@
 ;;; Soros Liu
 ;;; Exercise 2.62
-;;; 
+;;; https://sicp.sorosliu.xyz/book/book-Z-H-16.html#%_thm_2.62
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+          (let ((x1 (car set1)) (x2 (car set2)))
+            (cond ((= x1 x2)
+                   (cons x1 (union-set (cdr set1) (cdr set2))))
+                  ((< x1 x2)
+                   (cons x1 (union-set (cdr set1) set2)))
+                  ((< x2 x2)
+                   (cons x2 (union-set set1 (cdr set2)))))))))
+
+;;; verify
+(union-set '(0 1 2) '(0 1 2))
+;Value: (0 1 2)
+
+(union-set '(0 1 2) '(1 2 3))
+;Value: (0 1 2 3)
+
+(union-set '(0 1 2) '(2 3 4))
+;Value: (0 1 2 3 4)
+
+(union-set '(0 1 2) '(3 4 5))
+;Value: (0 1 2 3 4 5)
